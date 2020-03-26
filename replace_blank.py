eb5cf1841cbdfc21a249b3f6bfd411d28a6a2410
meglio <meglio@meglio.com>
2020-03-26 22:25:28 +0100

    replace improved for making the insertion of docs

--- replace_blank.py
+++ replace_blank.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import re
 import glob
 from os.path import join
@@ -22,7 +22,7 @@ class REP():
     def find_type(self):
         '''
         '''
-        dic_type = {'pdf':'§§','mp4':'%%'}
+        dic_type = {'pdf':'§§','mp4':'%%','txt':',,'}
         self.type = dic_type[self.name[-3:]]
         #print("self.type ",self.type)
 
@@ -66,12 +66,14 @@ class REP():
 
     def rm_ext(self):
         '''
+        Remove extension
         '''
         self.name = self.name[:-4]
         return self
 
     def rm_expr(self):
         '''
+        Remove a given expression
         '''
         if self.args.rm:
             self.name = self.name.replace(self.args.rm,'')
@@ -79,6 +81,7 @@ class REP():
 
     def mv(self):
         '''
+        Change from one ame to another..
         '''
         sh.move(self.name_orig,self.name)
 
@@ -87,7 +90,7 @@ class REP():
 def make_line(f):
     '''
     '''
-    # remove extension, make blanks, remove given expression, remove beginning not alphanum.. 
+    # remove extension, make blanks, remove given expression, remove beginning not alphanum..
     r = REP(f).rm_ext().mk_blk().rm_expr().cut_beg()
     newr = dict(r.__dict__,**r.args.__dict__)
     if newr['type'] == '**':
@@ -95,23 +98,42 @@ def make_line(f):
     line = '[{name} {type}]({name_orig})'.format(**newr)
     return line
 
+def print_reflnk():
+    '''
+    '''
+    try:
+        with open('ref.lnk','r') as g:
+            lines = g.readlines()
+            for l in lines:
+                print(l.strip())
+    except:
+        print('no ref.lnk file')
+
 def rep():
     '''
+    Clean the names and provide a list to glue in the straptoc doc..
     -"name artist" : remove "name artist" in the name
     '''
 
     #print('args is {} '.format(args))
-    lext = ['mp4','pdf','jpg']
+    lext = ['mp4','pdf','jpg','txt']
+    dic_prefix = {'pdf':'$pdf','mp4':'$vid'}
+    dic_score = {'mp4':0,'pdf':0,'jpg':0,'txt':0}
     for ext in lext:
       for f in glob.glob('*.' + ext):
         print(f)
-        # remove brackets, replace blanks, replace double points, change name..
-        r = REP(f).rm_brk().rep_blk().rep_dbpts().mv()
+        dic_score[ext] += 1
+        r = REP(f).rm_brk().rep_blk().rep_dbpts().mv() # remove brackets, replace blanks, replace double points, change name..
+    maxtype = max(dic_score, key=dic_score.get)
     print('-------------')   # show code to insert in the doc..
+    print('\t* ' + dic_prefix[maxtype])
+    print('\t+++ ' + os.getcwd().replace('/media/',''))
     for ext in lext:
       for f in glob.glob('*.' + ext):
           line = make_line(f)
-          print(line)
+          print('\t' + line)
+    print_reflnk()
+
 
 if __name__ == '__main__':
     rep()
