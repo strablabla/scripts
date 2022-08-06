07c74f7bdad31330c6399243ee6f4a8d098559e0
lio3867 <{lionel.chiron@gmail.com}>
2022-08-06 13:08:51 +0200

    folder for rep

--- replace_blank.py
+++ replace_blank.py
@@ -3,6 +3,7 @@ import re
 import glob
 from os.path import join
 ops = os.path.splitext
+opi = os.path.isdir
 import shutil as sh
 import argparse
 from datetime import datetime as dt
@@ -121,137 +122,203 @@ class REP():
 
 #####
 
-def make_line(f, debug=[]):
+class HANDLE_LINE():
     '''
     '''
-    # remove extension, make blanks, remove given expression, remove beginning not alphanum..
-    r = REP(f).rm_ext().mk_blk().rm_expr().cut_beg()
-    newr = dict(r.__dict__, **r.args.__dict__)
-    if newr['type'] == '**':
-        newr['type'] = r.type
+    def __init__(self):
+        '''
+        '''
 
-    pref0 = '!' if newr['type'] == '%' else ''
-    pref1 = '%' if newr['type'] == '%' else ''
-    if 0 in debug:
-        print(f"newr['type'] {newr['type']}")
-        print(f"newr['name'] {newr['name']}")
-    line = f'{pref0}[{pref1} {newr["name"]} {newr["type"]}]({newr["name_orig"]})'
-    return line
+    def make_line(self, f, debug=[]):
+        '''
+        '''
+        # remove extension, make blanks, remove given expression, remove beginning not alphanum..
+        r = REP(f).rm_ext().mk_blk().rm_expr().cut_beg()
+        newr = dict(r.__dict__, **r.args.__dict__)
+        if newr['type'] == '**':
+            newr['type'] = r.type
 
-def print_reflnk():
-    '''
-    '''
-    try:
-        with open('ref.lnk', 'r') as g:
-            lines = g.readlines()
-            for l in lines:
-                print(l.strip())
-    except:
-        print('no ref.lnk file')
+        pref0 = '!' if newr['type'] == '%' else ''
+        pref1 = '%' if newr['type'] == '%' else ''
+        if 0 in debug:
+            print(f"newr['type'] {newr['type']}")
+            print(f"newr['name'] {newr['name']}")
+        line = f'{pref0}[{pref1} {newr["name"]} {newr["type"]}]({newr["name_orig"]})'
+        return line
 
-def extract_date(name):
-    '''
-    '''
-    b = re.search('\d{4}-\d{2}-\d{2}', name)
-    try:
-        date = b.group(0)
-    except:
-        date = None
+    def print_reflnk(self, debug=[]):
+        '''
+        '''
+        try:
+            with open('ref.lnk', 'r') as g:
+                lines = g.readlines()
+                for l in lines:
+                    print(l.strip())
+        except:
+            if 0 in debug:
+                print('no ref.lnk file')
 
-    return date
+    def extract_date(self, name):
+        '''
+        '''
+        b = re.search('\d{4}-\d{2}-\d{2}', name)
+        try:
+            date = b.group(0)
+        except:
+            date = None
 
-def sort_by_date(l):
-    '''
+        return date
 
-    '''
-    lwithout_date = []
-    lwith_date = []
-    for name in l:
-        extr = extract_date(name)
-        if (not extr):
-            lwithout_date.append(name)
+    def sort_by_date(self, l):
+        '''
+
+        '''
+        lwithout_date = []
+        lwith_date = []
+        for name in l:
+            extr = self.extract_date(name)
+            if (not extr):
+                lwithout_date.append(name)
+            else:
+                lwith_date.append([dt.strptime(extr, "%Y-%m-%d"), name])
+        sorted_lwith_date = sorted(lwith_date, key=lambda x: x[1])              # sort on date the pairs (date,name)
+        sorted_list_name = [elem[1] for elem in sorted_lwith_date]                  # extract the name
+        full_list = sorted_list_name + lwithout_date
+        return full_list
+
+    def find_num(s):
+        '''
+        s : string
+        '''
+        name, ext = ops(s)
+        try:
+            num = int(re.findall('\d+', name)[0].lstrip('0'))
+            print(num)
+        except:
+            num = 0
+        return num
+
+    def sort_by_num(self, l):
+        '''
+        Sort the files with the number
+         inside..useful for lessons for example..
+        '''
+        # sort on number,name
+        sorted_with_num = sorted(l, key=lambda x: find_num(x))
+        return sorted_with_num
+
+    def sort_with_arg(self, lfiles):
+        '''
+        '''
+        r = REP(lfiles[0])
+        if r.args.date:
+            lsorted = self.sort_by_date(lfiles)  # sort by date
+        elif r.args.num:
+            lsorted = self.sort_by_num(lfiles)   # sort by num
         else:
-            lwith_date.append([dt.strptime(extr, "%Y-%m-%d"), name])
-    sorted_lwith_date = sorted(lwith_date, key=lambda x: x[1])              # sort on date the pairs (date,name)
-    sorted_list_name = [elem[1] for elem in sorted_lwith_date]                  # extract the name
-    full_list = sorted_list_name + lwithout_date
-    return full_list
+            lsorted = lfiles
 
-def find_num(s):
-    '''
-    s : string
-    '''
-    name, ext = ops(s)
-    try:
-        num = int(re.findall('\d+', name)[0].lstrip('0'))
-        print(num)
-    except:
-        num = 0
-    return num
+        return lsorted
 
-def sort_by_num(l):
-    '''
-    Sort the files with the number inside..useful for lessons for example..
-    '''
-    # sort on number,name
-    sorted_with_num = sorted(l, key=lambda x: find_num(x))
-    return sorted_with_num
 
-def sort_with_arg(lfiles):
-    '''
-    '''
-    r = REP(lfiles[0])
-    if r.args.date:
-        lsorted = sort_by_date(lfiles)  # sort by date
-    elif r.args.num:
-        lsorted = sort_by_num(lfiles)   # sort by num
-    else:
-        lsorted = lfiles
 
-    return lsorted
-
-def rep():
+class CLEAN_AND_CODE_STRAP(HANDLE_LINE):
     '''
-    Clean the names and provide a list to glue in the straptoc doc..
-    -"name artist" : remove "name artist" in the name
     '''
 
-    #print('args is {} '.format(args))
-    lext = ['mp3', 'mp4', 'avi', 'm4v', 'pdf', 'djvu', 'jpg', 'txt'] # authorized extensions
-    dic_prefix = {'pdf': '$pdf', 'djvu': '$pdf', 'mp4': '$vid',
-                  'avi': '$vid', 'm4v': '$vid', 'mp3': '$vid',
-                  'jpg': '$portf'}
-    dic_score = {'mp3': 0, 'mp4': 0, 'avi': 0, 'm4v': 0, 'pdf': 0,
-                 'djvu': 0, 'jpg': 0, 'txt': 0}
-    nbfiles = 0
-    for ext in lext:
-        ll = glob.glob(f'*.{ext}')
+    def __init__(self):
+        '''
+        '''
+        HANDLE_LINE.__init__(self)
+
+        self.dic_prefix = {'pdf': '$pdf', 'djvu': '$pdf', 'mp4': '$vid',
+                           'avi': '$vid', 'm4v': '$vid', 'mp3': '$vid',
+                           'jpg': '$portf'}
+        self.dic_score = {'mp3': 0, 'mp4': 0, 'avi': 0, 'm4v': 0, 'pdf': 0,
+                          'djvu': 0, 'jpg': 0, 'txt': 0}
+        self.lext = ['mp3', 'mp4', 'avi', 'm4v',
+                     'pdf', 'djvu', 'jpg', 'txt'] # authorized extensions
+
+    def clean_names(self, ll, ext, debug=[]):
+        '''
+        Adapt the names to nodestrap syntax.
+        '''
         for f in ll:
-            print(f)
-            dic_score[ext] += 1
+            if 0 in debug:
+                print(f)
+            self.dic_score[ext] += 1
             '''
             remove brackets, remove accents, remove patterns,
              replace blanks, replace double points, change name..
             '''
             r = REP(f).rm_brk().rm_acc().rm_patt().rep_blk().rep_dbpts().mv()
-        nbfiles += len(ll)
-    print(f'nb of files is {nbfiles}')
-    maxtype = max(dic_score, key=dic_score.get)
-    print('-------------')   # show code to insert in the doc..
-    try:
-        print('\t* ' + dic_prefix[maxtype])
-        print('\t+++ ' + os.getcwd().replace('/media/', ''))
-    except:
-        print('issue with dic_prefix')
-    for ext in lext:
-        lfiles = glob.glob(f'*.{ext}')
-        if lfiles:
-            lsorted = sort_with_arg(lfiles)
-            for f in lsorted:
-                line = make_line(f)
-                print('\t' + line)
-    print_reflnk()
+
+    def list_and_clean(self, fold='', debug=[]):
+        '''
+        '''
+
+        nbfiles = 0
+
+        for ext in self.lext:
+            ll = glob.glob(f'{fold}*.{ext}')
+            self.clean_names(ll, ext)
+            nbfiles += len(ll)
+        if 0 in debug:
+            print(f'nb of files is {nbfiles}')
+        maxtype = max(self.dic_score, key=self.dic_score.get)
+        pref_type = self.dic_prefix[maxtype]
+
+        return pref_type
+
+    def handle_folder(self, fold):
+        '''
+        '''
+        if len(fold) == 0:
+            self.begl = '\t\t'
+            print(f'\t* source :: ' )
+        else:
+            self.begl = '\t\t'
+            print(f'\t* {fold[:-1]} ' )
+
+    def code_for_nodestrap(self, pref_type, fold='', debug=[]):
+        '''
+        '''
+        if 0 in debug:
+            print('-------------')   # show code to insert in the doc..
+        self.handle_folder(fold)
+        try:
+            print(f'{self.begl}* ' + pref_type)
+            print(f'{self.begl}+++ ' + os.getcwd().replace('/media/', ''))
+        except:
+            print('issue with dic_prefix')
+        for ext in self.lext:
+            lfiles = glob.glob(f'{fold}*.{ext}')
+            if lfiles:
+                lsorted = self.sort_with_arg(lfiles)
+                for f in lsorted:
+                    line = self.make_line(f)
+                    print(f'{self.begl}' + line)
+        self.print_reflnk()
+
+    def rep(self, debug=[]):
+        '''
+        Clean the names and provide a list to glue in the straptoc doc..
+        -"name artist" : remove "name artist" in the name
+        '''
+
+        ldir = [d for d in os.listdir() if opi(d) ]
+        print(f'ldir = {ldir}')
+        ldir += ['']
+        for dd in ldir:
+            if 0 in debug:
+                print(f'######## dd is {dd}')
+            if len(dd)>1:
+                dd += '/'
+            pref_type = self.list_and_clean(fold=dd)
+            self.code_for_nodestrap(pref_type,fold=dd)
 
 
 if __name__ == '__main__':
-    rep()
+
+    cacs = CLEAN_AND_CODE_STRAP()
+    cacs.rep()
